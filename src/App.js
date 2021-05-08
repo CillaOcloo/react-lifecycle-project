58d226b394a0d6f8474e094576986c1e82f76bdd
Ocloo Priscilla <ocloopriscy98@gmail.com>
2021-05-08 16:44:43 +0000

    react lifecycle project initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import UserClass from  './Components/UsersClass';
+import UsersFunction from './Components/UsersFunction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <UserClass /> */}
+      <UsersFunction />
+      
+      
     </div>
+    
   );
 }
 
